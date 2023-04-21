6bc954469de3fa4a2f80b914b0881cbb74d11da4
mew <102923191+meeeww@users.noreply.github.com>
2023-04-22 00:31:04 +0200

    try deploy

--- index.js
+++ index.js
@@ -1,39 +1,21 @@
 const express = require('express')
-const cors = require('cors')
-const mysql = require('mysql')
-const bodyParser = require('body-parser')
 const app = express()
-app.use(cors())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json({ limit: '10mb' }))
-// const JWT = require('jsonwebtoken')
-// const secretWord = 'Samus#Aran'
 
-const credentials = {
-	host: '212.227.32.40',
-	user: 'root',
-	password: '8m!25i!17I',
-	database: 'bellum'
-}
+let users = [
+	{
+		id: 1,
+		nombre: "Juan"
+	}
+]
 
-app.get('/', (req, res) => {
-	res.send('hola desde tu primera rutda de la Api')
+app.get("/", (req, res) => {
+	res.send("deployado")
 })
 
-app.get('/api/login', (req, res) => {
-	console.log("hey")
-	const { username, password } = req.body
-	//const values = [username, password]
-	var connection = mysql.createConnection(credentials)
-	connection.query("SELECT * FROM contacto", (err, result) => {
-		if (err) {
-			res.status(500).send(err)
-		} else {
-			res.send(result)
-		}
-	})
-	connection.end()
+app.get("/api/users", (req, res) => {
+	res.json(users)
 })
 
-
-app.listen(3001, () => console.log('hola soy el servidor'))
+app.listen(3001, () => {
+	console.log("escuchando")
+})
