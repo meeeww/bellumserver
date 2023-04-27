2b1ed5f2356b549e9367ffdc519fb9fdf77bb925
mew <102923191+meeeww@users.noreply.github.com>
2023-04-27 03:29:16 +0200

    Login terminado

--- functions/api.js
+++ functions/api.js
@@ -26,6 +26,17 @@ let users = [
 	}
 ]
 
+function generate_token(length) {
+	//edit the token allowed characters
+	var a = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".split("");
+	var b = [];
+	for (var i = 0; i < length; i++) {
+		var j = (Math.random() * (a.length - 1)).toFixed(0);
+		b[i] = a[j];
+	}
+	return b.join("");
+}
+
 router.get("/users", (req, res) => {
 	res.json(users)
 })
@@ -41,6 +52,61 @@ router.get("/info", (req, res) => {
 	})
 })
 
+router.post("/login", (req, res) => {//encriptar + sql injection + cliente envia contraseña encriptada (en el server vuelves a encriptar)
+
+	const nombreInicio = req.body.nombreInicio
+	const contrasenaInicio = req.body.contrasenaInicio
+	const fecha = req.body.date
+	const agente = req.body.agente
+	const token = generate_token(128)
+
+	const sqlSelect = "SELECT * FROM `cuentas` WHERE nombreCuenta = ? AND contraCuenta = ?"
+	db.query(sqlSelect, [nombreInicio, contrasenaInicio], (err, result) => {
+		if (err) {
+			res.send(sqlSelect + " " + nombreInicio + " " + contrasenaInicio + " error")
+		} else {
+			const idUsuario = result[0]["id_cuenta"]
+
+			const sqlSelect = "INSERT INTO `sesiones` (`id_usuario`, `primeraSesion`, `ultimaSesion`, `dispositivo`, `token`) VALUES ('" + idUsuario + "', '" + fecha + "', '" + fecha + "', '" + agente + "', '" + token + "')"
+			db.query(sqlSelect, [idUsuario, fecha, fecha, agente, token], (err, result) => {
+				if (err) {
+					res.send(sqlSelect + " " + nombreInicio + " " + contrasenaInicio + " error2")
+				} else {
+					res.send(token)
+				}
+			})
+		}
+	})
+})
+
+router.post("/checksession", (req, res) => {//encriptar + sql injection + cliente envia contraseña encriptada (en el server vuelves a encriptar)
+
+	const token = req.body.token
+
+	const sqlSelect = "SELECT * FROM `sesiones` WHERE token = ?"
+	db.query(sqlSelect, [token], (err, result) => {
+		if (err) {
+			res.send(sqlSelect + " " + nombreInicio + " " + contrasenaInicio + " error")
+		} else {
+			res.send(result)
+		}
+	})
+})
+
+router.get("/persona/:idPersona", (req, res) => {
+
+	const idPersona = req.params.idPersona
+
+	const sqlSelect = "SELECT * FROM `cuentas` WHERE id_cuenta = " + idPersona
+	db.query(sqlSelect, (err, result) => {
+		if (err) {
+			res.send(sqlSelect + " " + idPersona + " error")
+		} else {
+			res.send(result)
+		}
+	})
+})
+
 router.post('/enviarmensaje', (req, res) => {
 
 	const nombreContacto = req.body.nombreContacto
@@ -49,14 +115,15 @@ router.post('/enviarmensaje', (req, res) => {
 	const asuntoContacto = req.body.asuntoContacto
 	const mensajeContacto = req.body.mensajeContacto
 
-	console.log("hey")
-	console.log(nombreContacto, apellidoContacto, correoContacto, asuntoContacto, mensajeContacto)
+	console.log(req.body)
+	//console.log(nombreContacto, apellidoContacto, correoContacto, asuntoContacto, mensajeContacto)
 
 	const sqlInsert = "INSERT INTO `contacto` (`id_contacto`, `nombre`, `apellido`, `email`, `asunto`, `mensaje`) VALUES (NULL, ?, ?, ?, ?, ?);"
 	console.log(sqlInsert)
 	db.query(sqlInsert, [nombreContacto, apellidoContacto, correoContacto, asuntoContacto, mensajeContacto], (err, result) => {
 		console.log(result)
 		res.status(200)
+		db.end()
 	})
 })
 
